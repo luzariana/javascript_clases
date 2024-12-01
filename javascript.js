bcc08e815b5c3a2e8a08527e22e4707aa7c6dc1b
Ariana Montes <ariana.montes@visitor.upm.com>
2024-12-01 14:09:08 -0300

    Clase 1y2 JS

---
+++ javascript.js
@@ -0,0 +1,29 @@
+let myName = "Ariana";
+
+let myLastname = "Montes"
+
+let myFullName1 = myName + " " + myLastname;
+
+console.log(myFullName1)
+
+let MyFullname2 = `${myName} ${myLastname}`
+
+console.log(MyFullname2)
+
+
+let myFirstNumber = 1
+myFirstNumber++;
+myFirstNumber--;
+
+let mySecondNumber = 2
+
+let myNewNumber = myFirstNumber + mySecondNumber
+
+myFirstNumber = myFirstNumber + 10;
+
+myFirstNumber += 10;
+
+console.log(myFirstNumber - mySecondNumber)
+console.log(myFirstNumber + mySecondNumber)
+console.log(myFirstNumber * mySecondNumber)
+console.log(myFirstNumber / mySecondNumber)
